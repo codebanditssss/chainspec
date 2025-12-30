bd8dcae388485d0dd8c1b7bd25c3ddfe12754d3d
Your Name <diwankhushi428@gmail.com>
2025-12-30 16:37:52 +0530

    .

--- dashboard/next.config.ts
+++ dashboard/next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactCompiler: true,
+  experimental: {
+    turbo: {
+      root: process.cwd(),
+    },
+  },
 };
 
 export default nextConfig;
